254847176c6f7c25b91d3c0e1c1aeee5fb5b602a
charstnut <36014603+charstnut@users.noreply.github.com>
2021-02-12 02:58:53 -0500

    Improves plotting in utils.

--- pyexp/test.py
+++ pyexp/test.py
@@ -1,28 +1,40 @@
 from pyexp import VCS_DIR
 import pyexp.utils as utils
 import os
-import sys
+from typing import Tuple, Union, List
 
 IMG_DIR = os.path.join(VCS_DIR, 'images')
-if not os.path.isdir(IMG_DIR):
-    print("Creating images directory")
-    try:
-        os.mkdir(IMG_DIR)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        raise
-
+utils.initialize_dir(IMG_DIR)
 utils.set_figure_style()
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 
-x = np.arange(1, 10)
-y = np.copy(x)
+x: np.ndarray = np.arange(1, 10)
+y: np.ndarray = np.copy(x)
 
-f = plt.figure()
-plt.plot(x, y)
+
+def plot_errorbar(x, y) -> Tuple[Figure, Union[Axes, List[Axes]]]:
+    # Plotting errorbar
+
+    fig, ax = plt.subplots()
+    fig.suptitle("")
+    title = "Name of The Axes"
+    ax.set_title(title)
+    error_kw = dict(marker='.', ls="None", capsize=5, capthick=2)
+    ax.errorbar(x,
+                y,
+                yerr=np.random.uniform(size=x.size),
+                label='Data',
+                **error_kw)
+    ax.set_xlabel(r"X Data $[A.U.]$")
+    ax.set_ylabel(r"Y Data $[A.U.]$")
+    ax.legend(loc="best")
+    return (fig, ax)
+
+
+fig, ax = plot_errorbar(x, y)
 plt.show()
-
-title = "Sample Figure"
-utils.save_fig(f, title, IMG_DIR)
+utils.save_fig(fig, IMG_DIR)
--- pyexp/utils.py
+++ pyexp/utils.py
@@ -2,10 +2,15 @@
 # For type checking
 from __future__ import annotations
 import os
+import sys
+from typing import Tuple, Union, List
 
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+
+import numpy as np
 
 
 def set_figure_style() -> None:
@@ -13,7 +18,7 @@ def set_figure_style() -> None:
     """
 
     plt.style.use(['seaborn-talk'])
-    FONT = {'family': 'STIXGeneral', 'size': 12}
+    FONT = {'family': 'STIXGeneral', 'size': 13}
     mpl.rc('font', **FONT)  # pass in the font dict as kwargs
     mpl.rc(('xtick', 'ytick'), labelsize=16, direction='in')
     mpl.rcParams['figure.autolayout'] = True
@@ -24,14 +29,63 @@ def set_figure_style() -> None:
     mpl.rcParams['axes.labelweight'] = 'bold'
     mpl.rcParams['lines.linewidth'] = 2
     mpl.rcParams['legend.fontsize'] = 13
+    mpl.rcParams["figure.figsize"] = [8.0, 8.0]
 
     print("Figure style set.")
 
 
-def save_fig(fig: Figure, title: str, image_dir: str) -> None:
+def save_fig(fig: Figure, image_dir: str, title: str = None) -> None:
     # Saves the figure to data/images with title and closes the figure.
 
+    # Default title to axes title if there's only one axes
+    if title is None:
+        ax_list = fig.get_axes()
+        if len(ax_list) == 1:
+            title = ax_list[0].get_title()
+        else:
+            raise ValueError("title cannot be None")
     out_file = os.path.join(image_dir,
                             title.replace(' ', '_').replace('.', '') + ".pdf")
     fig.savefig(out_file, bbox_inches='tight', dpi=300)
     plt.close(fig)
+
+
+def initialize_dir(path_name: str) -> bool:
+    # Initialize a directory
+
+    if not os.path.isdir(path_name):
+        print("Creating target [.../{}] directory... ".format(
+            os.path.split(path_name)[-1]))
+        try:
+            os.mkdir(path_name)
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        return True
+    else:
+        print("Target directory already exists.")
+        return True
+
+
+def sample_plot() -> Tuple[Figure, Union[Axes, List[Axes]]]:
+    # This is a sample script for plotting, not for direct use.
+    # Copy and modify the script and define plotting function separately.
+
+    # input x, y
+    x: np.ndarray = np.arange(1, 10)
+    y: np.ndarray = np.copy(x)
+
+    fig, ax = plt.subplots()
+    fig.suptitle("")
+    title = "Name of The Axes"
+    ax.set_title(title)
+    error_kw = dict(marker='.', ls="None", capsize=10, capthick=3)
+    ax.errorbar(x,
+                y,
+                yerr=np.random.uniform(size=x.size),
+                label='Data',
+                **error_kw)
+    ax.set_xlabel(r"Temp. $[^{\circ}C]$")
+    ax.set_ylabel(r"Signal Strength $[mV]$")
+    ax.legend(loc="best")
+    return (fig, ax)
